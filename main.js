e86e648671c1227e05d13ed9201189ba5a4f2288
Johnny Ung <johnnyung91@gmail.com>
2020-03-25 20:28:29 +0000

    added geocode function to return coordinates of lng and lat based on entered value in form

--- main.js
+++ main.js
@@ -6,33 +6,56 @@ var longitude = -118.012688;
 
 
 //DOM Queries
-
 var submitButton = document.getElementById("submit")
+var locationForm = document.getElementById('location-form');
 
+locationForm.addEventListener('submit', geocode);
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: { lat: latitude, lng: longitude }
-    });
+function geocode(e) {
+    e.preventDefault();
+    var location = document.getElementById("location-input").value;
     var geocoder = new google.maps.Geocoder();
-
-    document.getElementById("submit").addEventListener("submit", function() {
-        geocodeAddress(geocoder, map);
-    });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById("location-input").value;
-    geocoder.geocode({ address: address }, function(results, status) {
+    geocoder.geocode({"address": location}, function(results, status) {
         if (status === "OK") {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-        } else {
-            alert("Geocode was not successful for the following reason: " + status);
+            console.log(results[0].geometry.location.lat())
+            console.log(results[0].geometry.location.lng())
         }
-    });
+    })
+    console.log(location)
 }
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: latitude, lng: longitude},
+    zoom: 8
+  });
+}
+
+
+// function initMap() {
+//     var map = new google.maps.Map(document.getElementById('map'), {
+//       zoom: 8,
+//       center: {lat: -34.397, lng: 150.644}
+//     });
+//     var geocoder = new google.maps.Geocoder();
+
+//     document.getElementById('submit').addEventListener('click', function() {
+//       geocodeAddress(geocoder, map);
+//     });
+//   }
+
+//   function geocodeAddress(geocoder, resultsMap) {
+//     var address = document.getElementById('location-input').value;
+//     geocoder.geocode({'address': address}, function(results, status) {
+//       if (status === 'OK') {
+//         resultsMap.setCenter(results[0].geometry.location);
+//         var marker = new google.maps.Marker({
+//           map: resultsMap,
+//           position: results[0].geometry.location
+//         });
+//       } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//       }
+//     });
+//   }
