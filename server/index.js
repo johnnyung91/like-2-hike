656311f7b7c127f7f094b9ff8c90c03b28918226
Johnny Ung <johnnyung91@gmail.com>
2020-05-20 07:44:24 +0000

    able to host index.html file to local server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
+const path = require('path')
+const indexPath = path.join(__dirname, 'public/index.html')
+
 
 const app = express();
 
 app.use(staticMiddleware);
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(indexPath)
+})
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on port', process.env.PORT);
